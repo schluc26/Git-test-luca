60fe0dec4bc72ee4431cf7e80192dcdcdd04d885
Luca Schiavone <schluc26@ecolecatholique.ca>
2020-11-30 15:03:58 +0000

    update

--- formatif2/input_Luca.py
+++ formatif2/input_Luca.py
@@ -1,4 +1,6 @@
 #Ce fichier démontre input et opérations
 #LucaSchiavone1,schluc26@ecolecatholique.ca
 # 2020-11-30
-print("what is your name")
+print("Enter your name:")
+x = input("")
+print("Hello," + x)
