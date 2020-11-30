2c4ca1643a251b9ee748fed098c03fba253480e4
Luca Schiavone <schluc26@ecolecatholique.ca>
2020-11-30 14:53:18 +0000

    clean folders

---
+++ formatif1/hello_Luca.py
@@ -0,0 +1,6 @@
+ # Ce fichier demontre un message Hello World
+ #LucaSchiavone1,schluc26@ecolecatholique.ca
+# 2020-11-26
+print("Hello World")
+print("My name is Luca")
+print ("How are you doing?")
---
+++ formatif2/input_Luca.py
@@ -0,0 +1,4 @@
+#Ce fichier démontre input et opérations
+#LucaSchiavone1,schluc26@ecolecatholique.ca
+# 2020-11-30
+print("what is your name")
