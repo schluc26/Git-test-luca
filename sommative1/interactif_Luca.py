d5413a9c915c2a0e10fdb3bee3df5de6ed927f68
Luca Schiavone <schluc26@ecolecatholique.ca>
2020-12-04 16:16:09 +0000

    update

--- sommative1/interactif_Luca.py
+++ sommative1/interactif_Luca.py
@@ -14,12 +14,32 @@ elif y == "no":
 if y == "yes":
  print("question 1 of 3")
 print("what is the penguins real name a)andrew stone b)jack white c)oswald coblepott")
-questionanswer = input().lower()
-if questionanswer == "a":
+answer = input().lower()
+if answer == "a":
     print(" wrong")
-elif questionanswer == "b":
+elif answer == "b":
     print(" ha no")
-elif questionanswer == "c":
+elif answer == "c":
     print(" correct")
-if questionanswer == "c":
+if answer == "c":
   print("question 2 of 3")
+print("what is the best movie a) joker b) avengers endgame c) anything else")
+answer = input().lower()
+if answer == "a":
+    print(" correct correct correct")
+elif answer == "b":
+    print(" nice joke buddy")
+elif answer == "c":
+    print(" wrong!!")
+if answer == "a":
+  print("question 3 of 3")
+print("when did batman arkham origins take place a) june 5 b) december 24 to 25 c) october 31")
+answer = input().lower()
+if answer == "a":
+    print(" do you even know what arkham origins is")
+elif answer == "b":
+    print(" good job ")
+elif answer == "c":
+    print(" no no no")
+if answer == "b":
+  print("you won, thanks for playing")
