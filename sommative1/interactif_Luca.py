f5d850d0ddae853e022c40b3ae76b8f179d99d52
Luca Schiavone <schluc26@ecolecatholique.ca>
2020-12-04 14:44:29 +0000

    update

---
+++ sommative1/interactif_Luca.py
@@ -0,0 +1,13 @@
+#Programe interactif
+#Luca sommative
+print("what is your name")
+x = input()
+print("hello "+ x)
+print("do you wanr to play a game")
+y = input()
+yes = "good"
+no = "bad"
+if yes:
+  print("great lets start")
+elif no:
+  print("ok bye")
